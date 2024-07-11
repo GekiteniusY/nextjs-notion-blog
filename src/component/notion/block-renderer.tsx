a1f6964e6b4296fbbebadc769bfccbf7d5541d59
GekiteniusY <genkichi.yogi@gmail.com>
2024-07-11 23:05:02 +0900

    Blockレンダリングのコンポーネントの実装途中

--- src/component/notion/block-renderer.tsx
+++ src/component/notion/block-renderer.tsx
@@ -1,4 +1,5 @@
-import NotionText from "../notion-text";
+import NotionCode from "./notion-code";
+import NotionText from "./notion-text";
 
 type Block = {
   id: string;
@@ -7,10 +8,53 @@ type Block = {
 };
 
 // TODO: implement
+
+// Block objectのレンダー用関数
+// https://developers.notion.com/reference/block
+// アルファベット順
+
 export function renderBlock(block: Block) {
   const { id, blockType, value } = block;
 
   switch (blockType) {
+    // Bookmark
+    case "bookmark": {
+      return <a></a>;
+    }
+    // Breadcrumb(パンくずリスト)
+    case "breadcrumb": {
+      return <a></a>;
+    }
+    case "bulleted_list_item":{
+      return <a></a>
+    }
+    // https://extns.notion.site/d3d1b61749b942d592d69c9673ffb008
+    case "callout":{
+      return <a></a>
+    }
+    // TODO: サポート必要？？？
+    case "child_database":{
+      return <a></a>
+    }
+    case "child_page":
+      return (
+        <div>
+          <strong></strong>
+        </div>
+      );
+    // TODO: コードハイライトがされるようにする
+    case "code":
+      return (
+        <pre>
+          <NotionCode></NotionCode>
+        </pre>
+      );
+    case "column_list": {
+      return <div></div>;
+    }
+    case "column": {
+      return <div></div>;
+    }
     case "paragraph":
       return (
         <p>
@@ -18,18 +62,32 @@ export function renderBlock(block: Block) {
         </p>
       );
     case "heading_1":
-      return <h1></h1>;
+      return (
+        <h1>
+          <NotionText></NotionText>
+        </h1>
+      );
     case "heading_2":
-      return <h2></h2>;
+      return (
+        <h2>
+          <NotionText></NotionText>
+        </h2>
+      );
     case "heading_3":
-      return <h3></h3>;
+      return (
+        <h3>
+          <NotionText></NotionText>
+        </h3>
+      );
+    // TODO: bulleted_listは存在しないのでは？？？
     case "bulleted_list": {
       return <ul></ul>;
     }
     case "numbered_list": {
       return <ol></ol>;
     }
-    case "bulleted_list_item":
+    // TODO: 一緒の処理にするか後で検討する
+    // case "bulleted_list_item":
     case "numbered_list_item":
       return <li></li>;
     case "to_do":
@@ -46,12 +104,6 @@ export function renderBlock(block: Block) {
           <summary></summary>
         </details>
       );
-    case "child_page":
-      return (
-        <div>
-          <strong></strong>
-        </div>
-      );
     case "image": {
       return (
         <figure>
@@ -64,12 +116,6 @@ export function renderBlock(block: Block) {
       return <hr />;
     case "quote":
       return <blockquote></blockquote>;
-    case "code":
-      return (
-        <pre>
-          <code></code>
-        </pre>
-      );
     case "file": {
       return (
         <figure>
@@ -77,21 +123,12 @@ export function renderBlock(block: Block) {
         </figure>
       );
     }
-    case "bookmark": {
-      return <a></a>;
-    }
     case "table": {
       return (
         <table>
           <tbody></tbody>
         </table>
       );
-    }
-    case "column_list": {
-      return <div></div>;
-    }
-    case "column": {
-      return <div></div>;
     }
     default:
   }
---
+++ src/component/notion/notion-code.tsx
@@ -0,0 +1,8 @@
+// TODO: コードのハイライトがされるように実装する
+// 参考：Webページ上のコードブロックにシンタックスハイライトを適用する
+// https://qiita.com/kaiyo_yume/items/b003f2dbc049cacb6b47
+export default function NotionCode({ code }:any) {
+
+  
+    return (<code></code>);
+  }
